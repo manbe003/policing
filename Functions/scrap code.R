89b8d785975ca89a02c2435b1414bc1e7ad61225
khsavage <katiesavage2018@gmail.com>
2020-11-05 17:21:22 -0800

    a function attempt with bugs

--- Functions/scrap code.R
+++ Functions/scrap code.R
@@ -5,31 +5,21 @@ setwd("C:/Users/katie/Desktop/policing/clean data/Austin")
 Citations<-read.csv("Citations_Austin.csv")
 Shootings<-read.csv("Shootings_Austin.csv")
 UOF<-read.csv("UseOfForce_Austin.csv")
-Demodata<-read.csv("Austin_City_DemoData.csv")
 
-#get list of races
-listrace = unique(Citations$Subject.Race)
-
-#make an empty matrix to put values in
-db = matrix(ncol=length(listrace), nrow = length(Citations$Subject.Race))
-
-#loop over the races to preform perfect match test and insert them into our matrix
-for (i in 1:length(listrace)){
-  db[,i] = Citations$Subject.Race ==listrace[i]
+for (i in 1:length(listrace)) {
+  print(i)
 }
 
-#get count of incidents
-l=length(db[,1])
-
-#get count of success
-successes = table(db[,1])["TRUE"]
-
-#perform test with hardcoded demographic value
-binom.test(as.integer(successes), l, .34)
+for (i in 1:length(listrace)) {
+  db[i] = Citations$Subject.Race == listrace[i]
+}
 
 
-#creating function
-racecol<-as.data.frame
+
+racecol <- data.frame(Date=as.Date(character()),
+                 File=character(), 
+                 User=character(), 
+                 stringsAsFactors=FALSE)
 
 FunctionAustin = function(demodata, racecol){
   x=colnames(racecol)
@@ -46,10 +36,14 @@ FunctionAustin = function(demodata, racecol){
     successes = table(db[,1])["TRUE"]
     print(successes)
     print(listrace[i])
-  print(binom.test(as.integer(successes), l, demodata[i]))
+    print(binom.test(as.integer(successes), l, demodata[i]))
   }
 }
 
 FunctionAustin(demodata = Demodata, racecol = Citations$Subject.Race)
+
+
+
+
 
 

a9b8f7f59d726524119eed2b45f9acafe2ee99e6
Alexis Chouinard <alexischouinard@Alexiss-MacBook-Pro.local>
2020-11-15 13:48:22 -0800

    Indianapolis UOF EDA finished

--- EDA/Indianapolis/Indianapolis EDA.R
+++ EDA/Indianapolis/Indianapolis EDA.R
@@ -4,8 +4,58 @@ library(tidyr)
 library(tidyverse)
 
 #Set working directory
-setwd("~/Desktop/GitAndR/Policing/EDA/Indianapolis")
-
-#I want to call in my datasets (UOF)
+setwd("~/Desktop/GitAndR/Policing/clean data/Indianapolis")
+      
+#Call in my datasets (UOF)
 UOF<-read.csv(file='UOF.csv', stringsAsFactors = TRUE)
+
+##Descriptive data
+#Summary
+summary(UOF)
+
+#Standard deviations
+sd(UOF$residentAge, na.rm = TRUE)
+sd(UOF$officerAge, na.rm = TRUE)
+sd(UOF$officerYearsOfService, na.rm = TRUE)
+
+#Variation
+var(UOF$residentAge, na.rm = TRUE)
+var(UOF$officerAge, na.rm = TRUE)
+var(UOF$officerYearsOfService, na.rm = TRUE)
+
+##Graphical analysis
+#Outliers
+boxplot(UOF$residentAge, ylab = "Resident Age", main = "Boxplot Distribution of Resident Age in UOF")
+boxplot(UOF$officerAge, ylab = "Officer Age", main = "Boxplot Distribution of Officer Age in UOF")
+boxplot(UOF$officerYearsOfService, ylab = "Officer Years of Service", main = "Boxplot Distribution of Officer Years of Service in UOF")
+
+#Normality test
+ggplot(UOF, aes(sample = residentAge)) + geom_qq() + geom_qq_line()
+ggplot(UOF, aes(sample = officerAge)) + geom_qq() + geom_qq_line()
+ggplot(UOF, aes(sample = officerYearsOfService)) + geom_qq() + geom_qq_line()
+
+#Correlations
+cor.test(UOF$officerAge, UOF$residentAge)
+
+##Categorical data analysis
+#Levels
+levels(UOF$residentSex)
+levels(UOF$residentRace)
+levels(UOF$useOfForceReason)
+
+#Graphs
+ggplot(UOF, aes(residentSex)) + geom_bar()
+ggplot(UOF, aes(residentRace)) + geom_bar()
+ggplot(UOF, aes(officerSex)) + geom_bar()
+ggplot(UOF, aes(officerRace)) + geom_bar()
+ggplot(UOF, aes(useOfForceReason)) + geom_bar()
+ggplot(UOF, aes(division)) + geom_bar()
+
+#Comparison
+ggplot(UOF, aes(x = residentRace, fill = useOfForceReason)) + geom_bar(position = "dodge")
+ggplot(UOF, aes(x = residentRace, fill = officerRace)) + geom_bar(position = "dodge")
+ggplot(UOF, aes(x = residentRace, fill = division)) + geom_bar(position = "dodge")
+ggplot(UOF, aes(x = residentRace, fill = officerSex)) + geom_bar(position = "dodge")
+ggplot(UOF, aes(x = residentRace, fill = residentSex)) + geom_bar(position = "dodge")
+
 

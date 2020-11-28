351184de3fc56d8c1fb5c577466f4f224930a95f
khsavage <katiesavage2018@gmail.com>
2020-11-27 16:11:09 -0800

    another try at seperating rows for UOF officer data and shootings

--- EDA/Orlando/Orlando EDA.R
+++ EDA/Orlando/Orlando EDA.R
@@ -2,11 +2,11 @@
 library(tidyr)
 library(dplyr)
 library(here)
+library(tidyverse)
 
-
-##Categrical data because there is no descriptive data
+##Categorical data because there is no descriptive data
 #call files
-Shootings<-read.csv(file=here('clean data/orlando/Shooting (cleaned).csv'), stringsAsFactors = TRUE)
+Shootings<-read.csv(file=here('clean data/orlando/Shooting (cleaned).csv'), stringsAsFactors = FALSE)
 UOF<-read.csv(file=here('clean data/orlando/UOF (cleaned).csv'), stringsAsFactors = FALSE)
 
 #deleting rows where there are 6 races listed, but only 5 ethnicities listed.
@@ -14,8 +14,8 @@ UOF<- UOF[-c(3041, 4376, 5295, 1125), ]
 
 UOF<- UOF %>% separate_rows(Offenders.Race, Offenders.Sex, Offenders.Ethnicity, sep= ";")
 
-
-
+UOF_Officers<- UOF[-c(12,6,7,5,13,14,1,2,3,8,9,10,15,16,17,4,11,20,25,26,27,28,29,40,41,42,21,22,23,50,37,1936,45,49,24,37,32,33,34,35,51,52,48,46,31,38,30,18,43,44,53,39), ]
+UOF_tryin<- UOF %>% separate_rows(Officers.Race, Officers.Ethnicity, Officers.Sex, sep = ";")
 
 summary(UOF)
 summary(Shootings)
@@ -26,9 +26,21 @@ levels(UOF$Officers.Ethnicity)
 levels(UOF$Officers.Sex)
 levels(UOF$Offenders.Race)
 levels(UOF$Offenders.Ethnicity)
-levels(UOF$Offenders.Ethnicity)
 levels(UOF$Offenders.Sex)
 
 #graphs
-ggplot(UOF, aes(Officers.Race)) +
+ggplot(UOF, aes(Offenders.Race)) +
   geom_bar()
+
+ggplot(UOF, aes(Offenders.Ethnicity)) +
+  geom_bar()
+
+ggplot(UOF, aes(Offenders.Sex)) +
+  geom_bar()
+
+
+
+#############################
+Shootings<- Shootings %>% separate_rows(as.character(Suspect.Race), sep= ",")
+
+
--- clean data/orlando/comparing orlando.R
+++ clean data/orlando/comparing orlando.R
@@ -29,8 +29,8 @@ SplitDateTime_UOF<-as.data.frame(SplitDateTime_UOF, stringsAsFactors=FALSE)
 ##K: not sure exactly what this is doing
 NoTitle_UOF <- UOF_NA[UOF_NA$Incident.Date.Time != "", ]
 
-##seperating officer race into different officers
-shootings <-separate(shootings, Officer.Race, c("Officer One Race", "Officer Two Race", "Officer Three Race", "Officer Four Race" ), sep=", ")
+##separating officer race into different officers
+##K:shootings <-separate(shootings, Officer.Race, c("Officer One Race", "Officer Two Race", "Officer Three Race", "Officer Four Race" ), sep=", ")
 
 
 #making a table with all relevant metadata for UOF
@@ -79,8 +79,8 @@ AllMetadata_UOF_Fix$Offenders.Sex <- gsub('U', NA, AllMetadata_UOF_Fix$Offenders
 NoTitle_shootings <- shootings[shootings$Date != "", ]
 
 #making a table with all relevant metadata for shootings
-AllMetadata_shootings<-cbind.data.frame(NoTitle_shootings$ï..Case..,NoTitle_shootings$Date,NoTitle_shootings$`Officer One Race`,NoTitle_shootings$`Officer Two Race`,NoTitle_shootings$`Officer Three Race`,NoTitle_shootings$`Officer Four Race`,NoTitle_shootings$Ethnicity,NoTitle_shootings$Officer.Gender,NoTitle_shootings$Suspect.Race,NoTitle_shootings$Suspect.Gender,NoTitle_shootings$Suspect.Hit,NoTitle_shootings$Fatal, stringsAsFactors=FALSE)
-colnames(AllMetadata_shootings)<-(c("Incident Number","date","Officer One Race","Officer Two Race","Officer Three Race","Officer Four Race","Officer Ethnicity","Officer.Gender", "Suspect.Race","Suspect.Gender","Suspect.Hit","Fatal"))
+AllMetadata_shootings<-cbind.data.frame(NoTitle_shootings$ï..Case..,NoTitle_shootings$Date,NoTitle_shootings$Officer.Name,NoTitle_shootings$Officer.Race,NoTitle_shootings$Ethnicity,NoTitle_shootings$Officer.Gender,NoTitle_shootings$Suspect.Race,NoTitle_shootings$Suspect.Gender,NoTitle_shootings$Suspect.Hit,NoTitle_shootings$Fatal, stringsAsFactors=FALSE)
+colnames(AllMetadata_shootings)<-(c("Incident Number","date","Officer Name","Officer Race","Officer Ethnicity","Officer Gender", "Suspect.Race","Suspect.Gender","Suspect.Hit","Fatal"))
 
 #make all null values NA for shootings
 AllMetadata_shootings_NA<-AllMetadata_shootings
@@ -92,29 +92,11 @@ AllMetadata_shootings_NA[AllMetadata_shootings=="Omitted"]<-NA
 #Change race, ethnicity, sex to be consisted with shootings
 AllMetadata_shootings_Fix<-AllMetadata_shootings_NA
 
-AllMetadata_shootings_Fix$`Officer One Race` <- gsub('W', 'White', AllMetadata_shootings_Fix$`Officer One Race`)
-AllMetadata_shootings_Fix$`Officer One Race`<- gsub('A', 'Asian', AllMetadata_shootings_Fix$`Officer One Race`)
-AllMetadata_shootings_Fix$`Officer One Race`<- gsub('B', 'Black', AllMetadata_shootings_Fix$`Officer One Race`)
-AllMetadata_shootings_Fix$`Officer One Race`<- gsub('U', NA, AllMetadata_shootings_Fix$`Officer One Race`)
-AllMetadata_shootings_Fix$`Officer One Race`<- gsub('O', NA, AllMetadata_shootings_Fix$`Officer One Race`)
-
-AllMetadata_shootings_Fix$`Officer Two Race` <- gsub('W', 'White', AllMetadata_shootings_Fix$`Officer Two Race`)
-AllMetadata_shootings_Fix$`Officer Two Race`<- gsub('A', 'Asian', AllMetadata_shootings_Fix$`Officer Two Race`)
-AllMetadata_shootings_Fix$`Officer Two Race`<- gsub('B', 'Black', AllMetadata_shootings_Fix$`Officer Two Race`)
-AllMetadata_shootings_Fix$`Officer Two Race`<- gsub('U', NA, AllMetadata_shootings_Fix$`Officer Two Race`)
-AllMetadata_shootings_Fix$`Officer Two Race`<- gsub('O', NA, AllMetadata_shootings_Fix$`Officer Two Race`)
-
-AllMetadata_shootings_Fix$`Officer Three Race` <- gsub('W', 'White', AllMetadata_shootings_Fix$`Officer Three Race`)
-AllMetadata_shootings_Fix$`Officer Three Race`<- gsub('A', 'Asian', AllMetadata_shootings_Fix$`Officer Three Race`)
-AllMetadata_shootings_Fix$`Officer Three Race`<- gsub('B', 'Black', AllMetadata_shootings_Fix$`Officer Three Race`)
-AllMetadata_shootings_Fix$`Officer Three Race`<- gsub('U', NA, AllMetadata_shootings_Fix$`Officer Three Race`)
-AllMetadata_shootings_Fix$`Officer Three Race`<- gsub('O', NA, AllMetadata_shootings_Fix$`Officer Three Race`)
-
-AllMetadata_shootings_Fix$`Officer Four Race` <- gsub('W', 'White', AllMetadata_shootings_Fix$`Officer Four Race`)
-AllMetadata_shootings_Fix$`Officer Four Race`<- gsub('A', 'Asian', AllMetadata_shootings_Fix$`Officer Four Race`)
-AllMetadata_shootings_Fix$`Officer Four Race`<- gsub('B', 'Black', AllMetadata_shootings_Fix$`Officer Four Race`)
-AllMetadata_shootings_Fix$`Officer Four Race`<- gsub('U', NA, AllMetadata_shootings_Fix$`Officer Four Race`)
-AllMetadata_shootings_Fix$`Officer Four Race`<- gsub('O', NA, AllMetadata_shootings_Fix$`Officer Four Race`)
+AllMetadata_shootings_Fix$`Officer Race`<- gsub('W', 'White', AllMetadata_shootings_Fix$`Officer Race`)
+AllMetadata_shootings_Fix$`Officer Race`<- gsub('A', 'Asian', AllMetadata_shootings_Fix$`Officer Race`)
+AllMetadata_shootings_Fix$`Officer Race` <- gsub('B', 'Black', AllMetadata_shootings_Fix$`Officer Race`)
+AllMetadata_shootings_Fix$`Officer Race`<- gsub('U', NA, AllMetadata_shootings_Fix$`Officer Race`)
+AllMetadata_shootings_Fix$`Officer Race`<- gsub('O', NA, AllMetadata_shootings_Fix$`Officer Race`)
 
 AllMetadata_shootings_Fix$`Officer Ethnicity`<- gsub('N', 'Non-hispanic', AllMetadata_shootings_Fix$`Officer Ethnicity`)
 AllMetadata_shootings_Fix$`Officer Ethnicity`<- gsub('H', 'Hispanic', AllMetadata_shootings_Fix$`Officer Ethnicity`)
@@ -122,9 +104,9 @@ AllMetadata_shootings_Fix$`Officer Ethnicity`<- gsub('U', NA, AllMetadata_shooti
 AllMetadata_shootings_Fix$`Officer Ethnicity`<- gsub('W', 'White', AllMetadata_shootings_Fix$`Officer Ethnicity`)
 AllMetadata_shootings_Fix$`Officer Ethnicity`<- gsub('A', 'Asian', AllMetadata_shootings_Fix$`Officer Ethnicity`)
 
-AllMetadata_shootings_Fix$Officer.Gender <- gsub('M', 'Male', AllMetadata_shootings_Fix$Officer.Gender)
-AllMetadata_shootings_Fix$Officer.Gender <- gsub('F', 'Female', AllMetadata_shootings_Fix$Officer.Gender)
-AllMetadata_shootings_Fix$Officer.Gender <- gsub('U', NA, AllMetadata_shootings_Fix$Officer.Gender)
+AllMetadata_shootings_Fix$`Officer Gender` <- gsub('M', 'Male', AllMetadata_shootings_Fix$`Officer Gender`)
+AllMetadata_shootings_Fix$`Officer Gender` <- gsub('F', 'Female', AllMetadata_shootings_Fix$`Officer Gender`)
+AllMetadata_shootings_Fix$`Officer Gender` <- gsub('U', NA, AllMetadata_shootings_Fix$`Officer Gender`)
 
 AllMetadata_shootings_Fix$Suspect.Gender <- gsub('F', 'Female', AllMetadata_shootings_Fix$Suspect.Gender)
 AllMetadata_shootings_Fix$Suspect.Gender <- gsub('M', 'Male', AllMetadata_shootings_Fix$Suspect.Gender)

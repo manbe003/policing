5bf52d0264008fbec693277ccbb02cb24f919bbb
Katherine Manbeck <katherinemanbeck@Katherines-MacBook-Air.local>
2021-01-27 08:07:40 -0500

    deleting extra EDA files to combine all into the EDA script for Seattle

--- EDA/Seattle/EDA.R
+++ EDA/Seattle/EDA.R
@@ -6,7 +6,34 @@ library(tidyverse)
 #set working directory
 setwd("/Users/katherine/Policing/clean data/seattle")
 
-#I want to call in my datasets (shootings).
+#I want to call in my datasets (citations).
+UOF<-read.csv(file='UseOfForce_Seattle.csv', stringsAsFactors = TRUE)
+
+#summary data
+summary(UOF)
+
+#categorical data analysis
+levels(UOF$Subject_Gender)
+levels(UOF$Subject_Race)
+levels(UOF$Incident_Type)
+
+ggplot(UOF, aes(Subject_Gender))+
+  geom_bar()
+
+ggplot(UOF, aes(Subject_Race))+
+  geom_bar()
+
+ggplot(UOF, aes(Incident_Type))+
+  geom_bar()
+
+ggplot(UOF,
+       aes(x = Subject_Race,
+           fill = Incident_Type))+
+  geom_bar(position = "dodge")
+
+
+
+#shootings EDA
 shootings<-read.csv(file='shootings_Seattle.csv', stringsAsFactors = TRUE)
 
 #summary data
@@ -113,3 +140,79 @@ ggplot(shootings,
        aes(x = Subject.Race,
            fill = Subject.Weapon))+
   geom_bar(position = "dodge")
+
+
+
+
+#citations EDA
+#I want to call in my datasets (citations).
+citations<-read.csv(file='citations_Seattle.csv', stringsAsFactors = TRUE)
+
+#summary data
+summary(citations)
+
+#only numeric is YOB
+var(citations$Officer.YOB, na.rm=TRUE)
+sd(citations$Officer.YOB, na.rm=TRUE)
+
+#histogram of YOB
+hist(citations$Officer.YOB, breaks = 25)
+
+#boxplot
+boxplot(citations$Officer.YOB)
+
+#is age normally distributed
+ggplot(citations, aes(sample=Officer.YOB)) + 
+  geom_qq () +
+  geom_qq_line()
+#looks fairly normal except for a ceiling effect
+
+#categorical data analysis
+levels(citations$Subject.Age.Group)
+levels(citations$Stop.Resolution)
+levels(citations$Weapon.Type)
+levels(citations$Officer.Gender)
+levels(citations$Officer.Race)
+levels(citations$Subject.Perceived.Race)
+levels(citations$Subject.Perceived.Gender)
+levels(citations$Initial.Call.Type)
+levels(citations$Final.Call.Type)
+levels(citations$Call.Type)
+levels(citations$Arrest.Flag)
+levels(citations$Frisk.Flag)
+
+ggplot(citations, aes(Subject.Age.Group))+
+  geom_bar()
+
+ggplot(citations, aes(Stop.Resolution))+
+  geom_bar()
+
+ggplot(citations, aes(Weapon.Type))+
+  geom_bar()
+
+ggplot(citations, aes(Officer.Gender))+
+  geom_bar()
+
+ggplot(citations, aes(Officer.Race))+
+  geom_bar()
+
+ggplot(citations, aes(Subject.Perceived.Race))+
+  geom_bar()
+
+ggplot(citations, aes(Subject.Perceived.Gender))+
+  geom_bar()
+
+ggplot(citations, aes(Call.Type))+
+  geom_bar()
+
+ggplot(citations, aes(Arrest.Flag))+
+  geom_bar()
+
+ggplot(citations, aes(Frisk.Flag))+
+  geom_bar()
+
+ggplot(citations,
+       aes(x = Subject.Perceived.Race,
+           fill = Frisk.Flag))+
+  geom_bar(position = "dodge")
+

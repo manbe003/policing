e76d41179c288faffed8add0a3903c88a3464c28
khsavage <katiesavage2018@gmail.com>
2021-02-12 15:42:06 -0800

    data vis 2/12

--- Data Visualization/Orlando/Orlando Data Vizualization.R
+++ Data Visualization/Orlando/Orlando Data Vizualization.R
@@ -2,39 +2,50 @@
 library(ggplot2)
 library(here)
 library(dplyr)
+library(tidyr)
 
 #WD
 DemoData_UOF<-read.csv(file=here('clean data/orlando/Orlando City DemoData UOF.csv'), stringsAsFactors = FALSE)
 DemoData_Shootings<-read.csv(file=here('clean data/orlando/Orlando City DemoData Shootings.csv'), stringsAsFactors = FALSE)
-
-table(data2 <- data.frame(
-  Group=c("White","Black","other"),
-  Value=c("278105","0.24","0.395")
-  )
-  )
-
-ggplot(data2, aes(x="", y=Value, fill=Group)) +
-  geom_bar(stat="identity", width=1, color="white") +
-  coord_polar("y", start = 0)
+Shootings<-read.csv(file=here('clean data/orlando/shooting (cleaned).csv'), stringsAsFactors = FALSE)
 
 
-
-data2 = data.frame(count=c(192770,61670,957,9084,50,13574), category=c("white","black", "Native America","Asian","Native Hawaiian","Mixed"))
+Demodata = data.frame(count=c(192770,61670,957,9084,50,13574), category=c("white","black", "Native America","Asian","Native Hawaiian","Mixed"))
 
   
-data2$fraction = data2$count / sum(data2$count)
-data2 = data2[order(data2$fraction), ]
-data2$ymax = cumsum(data2$fraction)
-data2$ymin = c(0, head(data2$ymax, n=-1))
+Demodata$fraction = Demodata2$count / sum(Demodata$count)
+Demodata = Demodata[order(Demodata$fraction), ]
+Demodata$ymax = cumsum(Demodata2$fraction)
+Demodata$ymin = c(0, head(Demodata$ymax, n=-1))
 
 
-ggplot(data2, aes(fill=category, ymax=ymax, ymin=ymin, xmax=4, xmin=3)) +
+ggplot(Demodata, aes(fill=category, ymax=ymax, ymin=ymin, xmax=4, xmin=3)) +
   geom_rect() +
   coord_polar(theta="y") +
   xlim(c(0, 4)) +
   labs(title="Basic ring plot")
 
 
+#separating rows so there is only one offender per row
+Shootings_Offenders<- Shootings %>% separate_rows(Suspect.Race, Suspect.Gender, Suspect.Hit, Fatal, sep= ",")
 
+shootings_table<- table(Shootings_Offenders$Suspect.Race)
+shootings_table
+sum(shootings_table == White)
+
+
+
+
+ggplot(Shootings_Offenders2=data.frame(Suspect.Race), 
+       aes(x = factor(1),fill = factor(Suspect.Race))) + 
+  geom_bar(stat = "count") + 
+  scale_y_continuous(breaks = seq(0,12,3), labels = c("0", "25%", "50%", "75%", "100%")) + 
+  coord_polar(theta='y') +
+  coord_polar(theta='y') +
+  theme(axis.text.y = element_blank(), 
+        axis.title.y = element_blank(), 
+        axis.ticks.y = element_blank(),
+        axis.title.x = element_blank()) +
+  labs(fill = "Suspect.Race")
 
 

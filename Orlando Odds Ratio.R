2fa72962bf6eef84ca3c14f7a964c7b9946405d1
khsavage <katiesavage2018@gmail.com>
2021-04-30 14:29:27 -0700

    odds ratios fixed and ORs and Graphs for each type of UOF

--- Orlando Odds Ratio.R
+++ Orlando Odds Ratio.R
@@ -19,7 +19,7 @@ UOF_levels$Physical.Strikes.Made[UOF_levels$Physical.Strikes.Made=="Yes"]<-("3")
 UOF_levels$Deflation.Devices.Used[UOF_levels$Deflation.Devices.Used=="Yes"]<-("4")
 UOF_levels$K9.Unit.Involved[UOF_levels$K9.Unit.Involved=="Yes"]<-("4")
 UOF_levels[UOF_levels=="No"]<-NA
-UOF_levels$Offenders.Race[UOF_levels$Offenders.Race=="Asian"]<-NA
+
 
 #making one row with UOF used
 UOF_levels<- UOF_levels %>% unite("UOF.Level", Electronic.Device.Used:K9.Unit.Involved, sep = ";", na.rm = TRUE, remove = TRUE)
@@ -38,39 +38,162 @@ Victim_Levels<- as.data.frame(VictimRace_UOFLevel %>% group_by(V2,V1) %>% summar
 Race_Level<- VictimRace_UOFLevel %>% unite("Race and Level", V1:V2, sep = ", ")
 Victim_Levels2<- as.data.frame(Race_Level %>% group_by(`Race and Level`) %>% summarise(count = length(`Race and Level`[!is.na(`Race and Level`)])))
 
-
+#making a table of Level of force used with each race
 Level.Race<-table(VictimRace_UOFLevel$V2, VictimRace_UOFLevel$V1)
+Level.Race <- Level.Race[c(3,1:2),]
+print(Level.Race)
 
-Level.Race2<-matrix(Level.Race, nrow=2)
-rownames(Level.Race2)<-c("Black", "White")
-BW_OR<-oddsratio(Level.Race2)
+###Odds Ratio
+Race_OR<-oddsratio(Level.Race)
+#printing the outcome so its easier to read
+print(Race_OR$measure)
+print(Race_OR$p.value)
 
+#graph
+ggplot(VictimRace_UOFLevel,
+       aes(x = V1,
+           fill = V2))+
+  geom_bar(position = "dodge")
+
+
+##Odds ratio for group sizes
 
 #making the number of officers row <2 or 3+
 UOF_Officers<- UOF_levels
 UOF_Officers$Officers.Involved <- gsub('3|4|5|6|7|8|9|10|12','3+',UOF_Officers$Officers.Involved)
-#UOF_Officers$Officers.Involved <- gsub("1|2",'<2',UOF_Officers$Officers.Involved)
+UOF_Officers2<- UOF_Officers
+UOF_Officers$Officers.Involved <- gsub("1|2",'<2',UOF_Officers$Officers.Involved)
 
 #making a dataframe of just the UOF levels and Group Sizes
-UOF_GroupsandLevels <-as.data.frame(na.omit(cbind(as.character(UOF_Officers$Officers.Involved), as.character(UOF_Officers$UOF.Level))))
+UOF_GroupsandLevels <-as.data.frame(na.omit(cbind(as.character(UOF_Officers$UOF.Level), as.character(UOF_Officers$Officers.Involved))))
+UOF_Groupsandlevels2 <-as.data.frame(na.omit(cbind(as.character(UOF_Officers2$UOF.Level), as.character(UOF_Officers2$Officers.Involved))))
+
+#making a table of Group size and UOF level
+Levels.Groups<-table(UOF_GroupsandLevels$V1,UOF_GroupsandLevels$V2)
+print(Levels.Groups)
+
+Levels.Groups2<-table(UOF_Groupsandlevels2$V1,UOF_Groupsandlevels2$V2)
+print(Levels.Groups2)
+
+###Odds Ratio of <2 and 3+
+Groups.OR<-oddsratio(Levels.Groups)
+print(Groups.OR$measure)
+print(Groups.OR$p.value)
+
+###Odds ratio of 1, 2, and 3+ officers
+Groups.OR2<-oddsratio(Levels.Groups2)
+print(Groups.OR2$measure)
+print(Groups.OR2$p.value)
+
+#Graph
+ggplot(UOF_GroupsandLevels,
+       aes(x = V1,
+           fill = V2))+
+  geom_bar(position = "dodge")
+
+ggplot(UOF_Groupsandlevels2,
+       aes(x = V1,
+           fill = V2))+
+  geom_bar(position = "dodge")
 
 
-Levels.Groups<-table(UOF_GroupsandLevels$V2,UOF_GroupsandLevels$V1)
-print(table(UOF_GroupsandLevels$V2,UOF_GroupsandLevels$V1))
 
-Level.Groups2<-matrix(Levels.Groups, nrow=2)
-rownames(Level.Groups2)<-c("Level 3", "Level 4")
-BW_OR2<-oddsratio(Level.Groups2)
-
-
+#doing odds ratios for each UOF for each race and Groups
+UOF_Types<- UOF
+UOF_Types$Officers.Involved <- gsub('3|4|5|6|7|8|9|10|12','3+',UOF_Types$Officers.Involved)
+#UOF_Types$Officers.Involved <- gsub("1|2",'<2',UOF_Types$Officers.Involved)
+UOF_Types <- UOF_Types %>% separate_rows(Offenders.Race, sep = ";")
 
 
+#Taser and Race to double check function
+#Taser_Race <- as.data.frame(na.omit(cbind(as.character(UOF_Types$Offenders.Race), as.character(UOF_Types$Electronic.Device.Used))))
+#Taser_RaceCounts<- table(Taser_Race$V1,Taser_Race$V2)
+#Taser_RaceCounts <- Taser_RaceCounts[c(3,1:2),]
+#Taser_RaceCounts <- Taser_RaceCounts[,c(2,1)]
+#print(Taser_RaceCounts)
 
+#TaserRace.OR<-oddsratio(Taser_RaceCounts)
+#print(TaserRace.OR$measure)
+#print(TaserRace.OR$p.value)
 
+#ggplot(Taser_Race,
+ #      aes(x = V2,
+  #        fill = V1))+
+  #geom_bar(position = "dodge")
 
+##Taser and Groups to double check function
+#Taser_Groups <- as.data.frame(na.omit(cbind(as.character(UOF_Types$Officers.Involved), as.character(UOF_Types$Electronic.Device.Used))))
+#Taser_GroupsCounts<- table(Taser_Groups$V1,Taser_Groups$V2)
+#print(Taser_GroupsCounts)
 
+#TaserRace.OR<-oddsratio(Taser_GroupsCounts)
+#print(TaserRace.OR$measure)
+#print(TaserRace.OR$p.value)
 
+#ggplot(Taser_Groups,
+       #aes(x = V2,
+           #fill = V1))+
+  #geom_bar(position = "dodge")
 
+Function_UOFType = function(Data){
+  Type<- as.data.frame(na.omit(cbind(as.character(UOF_Types$Offenders.Race), as.character(Data))))
+  TypeCounts<- table(Type$V1,Type$V2)
+  TypeCounts <- TypeCounts[c(3,1:2),]
+  TypeCounts <- TypeCounts[,c(2,1)]
+  print(TypeCounts)
 
+  Type.OR<-oddsratio(TypeCounts)
+  print(Type.OR$measure)
+  print(Type.OR$p.value)
 
+  ggplot(Type,
+        aes(x = V2,
+             fill = V1))+
+   geom_bar(position = "dodge")
+  
+}
+
+Function_TypesGroups = function(Data){
+  Groups <- as.data.frame(na.omit(cbind(as.character(UOF_Types$Officers.Involved), as.character(Data))))
+  GroupsCounts<- table(Groups$V1,Groups$V2)
+  GroupsCounts <- GroupsCounts[,c(2,1)]
+  print(GroupsCounts)
+  
+  Group.OR<-oddsratio(GroupsCounts)
+  print(Group.OR$measure)
+  print(Group.OR$p.value)
+  
+  ggplot(Groups,
+         aes(x = V2,
+             fill = V1))+
+    geom_bar(position = "dodge")
+}
+
+#Taser
+Function_UOFType(Data = UOF_Types$Electronic.Device.Used)
+Function_TypesGroups(Data = UOF_Types$Electronic.Device.Used)
+
+#chemical Weapon
+Function_UOFType(Data = UOF_Types$Chemical.Agent.Used)
+Function_TypesGroups(Data = UOF_Types$Chemical.Agent.Used)
+
+#Tackle
+Function_UOFType(Data = UOF_Types$Tackle.Take.Down)
+Function_TypesGroups(Data = UOF_Types$Tackle.Take.Down)
+
+#Impact Weapon
+Function_UOFType(Data = UOF_Types$Impact.Weapons.Used)
+Function_TypesGroups(Data = UOF_Types$Impact.Weapons.Used)
+
+#Physical Strikes
+Function_UOFType(Data = UOF_Types$Physical.Strikes.Made)
+Function_TypesGroups(Data = UOF_Types$Physical.Strikes.Made)
+
+#Deflation Device
+Function_UOFType(Data = UOF_Types$Deflation.Devices.Used)
+Function_TypesGroups(Data = UOF_Types$Deflation.Devices.Used)
+
+#K9
+Function_UOFType(Data = UOF_Types$K9.Unit.Involved)
+Function_TypesGroups(Data = UOF_Types$K9.Unit.Involved)
 

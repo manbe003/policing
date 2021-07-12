e097eebb4b370ba694c8e250b3b224a75c3bb8d4
khsavage <katiesavage2018@gmail.com>
2021-07-12 16:16:47 -0700

    little more data cleaning

--- clean data/New Orleans/Cleaning New Orleans.R
+++ clean data/New Orleans/Cleaning New Orleans.R
@@ -1,9 +1,16 @@
 #loading libraries
 library(here)
 library(dplyr)
+library(tidyr)
 
 #loading datasets
 UOF <- read.csv(file=here('dirty data/New Orleans/NOPD_Use_of_Force_Incidents.csv'), stringsAsFactors = FALSE)
 
 #making a table with relevant data
-AllMetadata_UOF<- cbind.data.frame(UOF$PIB.File.Number,UOF$Date.Occurred,UOF$Originating.Bureau,UOF$Disposition,UOF$Service.Type.UOF)
+AllMetadata_UOF<- cbind.data.frame(UOF$PIB.File.Number,UOF$Date.Occurred,UOF$Disposition,UOF$Service.Type,UOF$Use.of.Force.Type,UOF$Use.of.Force.Level,UOF$Officer.Race.Ethnicity,UOF$Officer.Gender,UOF$Officer.Age,UOF$Officer.Years.of.Service,UOF$Subject.Gender,UOF$Subject.Ethnicity,UOF$Subject.Age,UOF$Subject.Injured,UOF$Use.of.Force.Reason,UOF$Officer.Injured, stringsAsFactors=FALSE)
+colnames(AllMetadata_UOF)<-(c("PIB File Number","Date","Disposition","Service Type","Force Type","Force Level","Officer Race","Officer Gender","Officer Age","Officers Yrs of Service","Subject Gender","Subject Race","Subject Age","Subject Injured","UOF Reason","Officer Injured"))
+
+
+#separating rows to be one offender per row
+UOF_Officers <- AllMetadata_UOF
+UOF_Offiers<- UOF_Officers %>% separate_rows("Officer Race", "Officer Gender", "Officer Age","Officers Yrs of Service",  sep= "|")

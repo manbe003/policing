f9063eb22cc4c971450a5259f649f25aba610374
khsavage <katiesavage2018@gmail.com>
2021-08-06 20:30:48 -0700

    making the function for ORs

--- analyses/Northampton/Northampton ORs.R
+++ analyses/Northampton/Northampton ORs.R
@@ -5,15 +5,21 @@ library(epitools)
 #loading datasets
 UOF<-read.csv(file=here('clean data/Northampton/Northampton UOF.csv'), stringsAsFactors = FALSE)
 
-#making dataframe of force levels and officer group sizes
-GroupSize_UOFLevel <- as.data.frame(na.omit(cbind(as.character(UOF$PD.Force.Type), as.character(UOF$Number.of.Officers))))
-
+#referring to which variable you want to be the row and column of the odds ratio
+OR_Function = function(rows,columns){
 #making a table of Level of force used with each race
-Level.Groups<-table(UOF$Number.of.Officers, UOF$PD.Force.Type)
-print(Level.Groups)
+OR_Table<-table(rows, columns)
+print(OR_Table)
 
 ###Odds Ratio
-Size_OR<-oddsratio(Level.Groups)
+OR<-oddsratio(OR_Table)
 #printing the outcome so its easier to read
-print(Size_OR$measure)
-print(Size_OR$p.value)
+print(OR$measure)
+print(OR$p.value)
+
+}
+
+OR_Function(UOF$Number.of.Officers, UOF$PD.Force.Type)
+
+
+

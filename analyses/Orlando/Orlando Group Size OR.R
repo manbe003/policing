0afe8fe23c5407f7eb87c9d704672c3b05070f84
khsavage <katiesavage2018@gmail.com>
2021-08-23 17:48:33 -0700

    doing the group size ORs for orlando properly

---
+++ analyses/Orlando/Orlando Group Size OR.R
@@ -0,0 +1,62 @@
+Libraries
+library(here)
+library(tidyverse)
+
+#loading in datasets
+UOF <- read.csv(file = here("clean data/Orlando/UOF (cleaned).csv"), stringsAsFactors = FALSE)
+OIS <- read.csv(file = here("clean data/Orlando/shooting (cleaned).csv"), stringsAsFactors = FALSE)
+
+
+OR_Prep = function(dataset,column){
+  #making a column binning level of force as lethal vs non lethal
+  dataset['Lethal.vs.Non-lethal.Weapon'] <- column
+  dataset$`Lethal.vs.Non-lethal.Weapon`<- gsub('1|2', 'Non-Lethal', dataset$`Lethal.vs.Non-lethal.Weapon`)
+  dataset$`Lethal.vs.Non-lethal.Weapon`<- gsub('3', 'Lethal', dataset$`Lethal.vs.Non-lethal.Weapon`)
+  
+  #making a column binning level of force as Weapon vs No Weapon
+  dataset['Weapon.vs.Weapon'] <- column
+  dataset$`Weapon.vs.Weapon`<- gsub('2|3', 'Weapon', dataset$`Weapon.vs.Weapon`)
+  dataset$`Weapon.vs.Weapon`<- gsub('1', 'No Weapon', dataset$`Weapon.vs.Weapon`)
+  return(dataset)
+}
+
+UOF <- OR_Prep(UOF,UOF$UOF.Level)
+
+
+#2nd function (referring to which variable you want to be the rows and columns of the odds ratio)
+OR_Function = function(row,column1,column2){
+  
+  #making a table of Lethal vs non lethal used with each race
+  OR_Table<-table(row, column1)
+  OR_Table <- OR_Table[c(3,1:2),]
+  print(OR_Table)
+  
+  ###Odds Ratio
+  OR<-oddsratio(OR_Table)
+  #printing the outcome so its easier to read
+  print(OR$measure)
+  print(OR$p.value)
+  
+  #making a table of Weapon vs No Weapon used with each race
+  OR_Table2<-table(row, column2)
+  OR_Table2 <- OR_Table2[c(3,1:2),]
+  OR_Table2<- OR_Table2[,c(2,1)]
+  print(OR_Table2)
+  
+  ###Odds Ratio
+  OR2<-oddsratio(OR_Table2)
+  #printing the outcome so its easier to read
+  print(OR2$measure)
+  print(OR2$p.value) 
+  
+  
+}
+
+OR_Function(UOF$Binning.Number.of.Officers,UOF$Weapon.vs.Weapon,UOF$Weapon.vs.Weapon)
+
+ggplot(UOF,
+       aes(x = Binning.Number.of.Officers,
+           fill = Weapon.vs.Weapon))+
+  geom_bar(position = "dodge")
+
+

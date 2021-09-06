0ecb0e32345dfd28255bc26e7162f650a51042a4
Dick McManus <dickmcmanus@gmail.com>
2021-09-06 12:48:48 -0400

    Updated Orlando dependency code

--- Data Cleaning Functions.R
+++ Data Cleaning Functions.R
@@ -1,5 +1,5 @@
 #create nulls
 ReplwNull = function(x){
-  match = c("","Unknown","unknwon","Not Applicable","NA")
+  match = c("","Unknown","unknwon","Not Applicable","NA","NULL")
   clean = mutate_all(x, list(~na_if_in(.,match)))
 }

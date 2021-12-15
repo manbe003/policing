26c3b587971875c74a02c5d8f53fcb6331ddaddc
khsavage <katiesavage2018@gmail.com>
2021-12-15 13:35:54 -0800

    group composition OR with 0%, 1-99%, and 100% groups

--- analyses/New Orleans/OfficerCompositionOR.R
+++ analyses/New Orleans/OfficerCompositionOR.R
@@ -40,18 +40,19 @@ dataset$percent.white <- dataset$percent.white * 100
 dataset$percent.white <- round(dataset$percent.white, digits = 0)
 
 #get rid of 1 person groups
-dataset$percent.white[dataset$Number.of.Officers == 1] <- NA
-dataset$percent.white[dataset$Number.of.Officers == 1] <- NA
+dataset1<-dataset
+dataset1$percent.white[dataset1$Number.of.Officers == 1] <- NA
+dataset1$percent.white[dataset1$Number.of.Officers == 1] <- NA
 
 #Binning percentages to make the OR easier
 #Binning into 0% = 0, 1%-25%, 26%-50% = 2, 50% , 51%-75%, 76%-99%, 100%
-dataset$Binning.Percent.White[dataset$percent.white == 0] <- "0%"
-dataset$Binning.Percent.White[dataset$percent.white >0 & dataset$percent.white <= 25] <- "1-25%"
-dataset$Binning.Percent.White[dataset$percent.white >25 & dataset$percent.white < 50] <- "26-49%"
-dataset$Binning.Percent.White[dataset$percent.white == 50] <- "50%"
-dataset$Binning.Percent.White[dataset$percent.white >50 & dataset$percent.white <= 75] <- "51-75%"
-dataset$Binning.Percent.White[dataset$percent.white >75 & dataset$percent.white <= 99] <- "76-99%"
-dataset$Binning.Percent.White[dataset$percent.white == 100] <- "100%"
+dataset1$Binning.Percent.White[dataset1$percent.white == 0] <- "0%"
+dataset1$Binning.Percent.White[dataset1$percent.white >0 & dataset1$percent.white <= 25] <- "1-25%"
+dataset1$Binning.Percent.White[dataset1$percent.white >25 & dataset1$percent.white < 50] <- "26-49%"
+dataset1$Binning.Percent.White[dataset1$percent.white == 50] <- "50%"
+dataset1$Binning.Percent.White[dataset1$percent.white >50 & dataset1$percent.white <= 75] <- "51-75%"
+dataset1$Binning.Percent.White[dataset1$percent.white >75 & dataset1$percent.white <= 99] <- "76-99%"
+dataset1$Binning.Percent.White[dataset1$percent.white == 100] <- "100%"
 
 
 #doing OR prep making it less lethal vs lethal and weapon vs no weapon
@@ -69,11 +70,11 @@ OR_Prep = function(dataset,column){
   return(dataset)
 }
 
-dataset <- OR_Prep(dataset,dataset$Force.Type)
+dataset1 <- OR_Prep(dataset1,dataset1$Force.Type)
 
 
 #only doing weapon/no weapon OR bc lethal/non lethal has to many 0s to compute
-OR_Table<-table(dataset$Binning.Percent.White, dataset$`Weapon.vs.No Weapon`)
+OR_Table<-table(dataset1$Binning.Percent.White, dataset1$`Weapon.vs.No Weapon`)
 OR_Table <- OR_Table[c(3,1:2,4:7),]
 print(OR_Table)
 
@@ -87,34 +88,34 @@ print(OR$p.value)
 
 #graph comparing the variables
 
-ggplot(dataset,
+ggplot(dataset1,
        aes(x = Binning.Percent.White,
            fill = as.character(Force.Type)))+
   geom_bar(position = "dodge")
 
-ggplot(dataset,
+ggplot(dataset1,
        aes(x = Binning.Percent.White,
            fill = `Lethal.vs.Non-lethal.Weapon`))+
   geom_bar(position = "dodge")
 
 
-ggplot(dataset,
+ggplot(dataset1,
        aes(x = Binning.Percent.White,
            fill = `Weapon.vs.No Weapon`))+
   geom_bar(position = "dodge")
 
 
 #doing the same thing but binning it as <50% and >50%
-
-dataset$Binning.Percent.White[dataset$percent.white < 50] <- "<50%"
-dataset$Binning.Percent.White[dataset$percent.white == 50] <- "50%"
-dataset$Binning.Percent.White[dataset$percent.white >50] <- ">50%"
+dataset2<- dataset1
+dataset2$Binning.Percent.White[dataset2$percent.white < 50] <- "<50%"
+dataset2$Binning.Percent.White[dataset2$percent.white == 50] <- "50%"
+dataset2$Binning.Percent.White[dataset2$percent.white >50] <- ">50%"
 
 
 #OR with new Binning
 #only doing weapon OR because lethal OR has to many 0s to compute
   #making a table of Lethal vs non lethal used with each race
-  OR_Table<-table(dataset$Binning.Percent.White, dataset$`Weapon.vs.No Weapon`)
+  OR_Table<-table(dataset2$Binning.Percent.White, dataset2$`Weapon.vs.No Weapon`)
   OR_Table <- OR_Table[c(3,1:2),]
   print(OR_Table)
   
@@ -125,4 +126,39 @@ dataset$Binning.Percent.White[dataset$percent.white >50] <- ">50%"
   print(OR$p.value)
   
 
+
+  
+##Doing it again but binning it as 0%,1-99%, and 100% white groups
+dataset3<- dataset
+dataset3$Binning.Percent.White[dataset3$percent.white >=1 & dataset3$percent.white <=99 ] <- "1-99%"
+dataset3$Binning.Percent.White[dataset3$percent.white == 0] <- "0%"
+dataset3$Binning.Percent.White[dataset3$percent.white == 100] <- "100%"
+
+dataset3 <- OR_Prep(dataset3,dataset3$Force.Type)
+
+  
+#making a table of Lethal vs non lethal used with each race
+OR_Table<-table(dataset3$Binning.Percent.White, dataset3$`Weapon.vs.No Weapon`)
+print(OR_Table)
+  
+###Odds Ratio
+OR<-oddsratio(OR_Table)
+#printing the outcome so its easier to read
+print(OR$measure)
+print(OR$p.value)
+
+#graphs
+  
+ggplot(dataset3,
+       aes(x = Binning.Percent.White,
+           fill = as.character(Force.Type)))+
+  geom_bar(position = "dodge")
+
+
+ggplot(dataset3,
+       aes(x = Binning.Percent.White,
+           fill = `Weapon.vs.No Weapon`))+
+  geom_bar(position = "dodge")
+
+  
 
